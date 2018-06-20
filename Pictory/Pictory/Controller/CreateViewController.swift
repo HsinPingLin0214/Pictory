16aa09ad79a80ec7fa192043de6f1bb3839d6e77
Jenna <jenna.lin@localz.co>
2018-06-21 00:03:41 +1000

    Implement Calendar.
    Stuck on range selected

--- Pictory/Pictory/Controller/CreateViewController.swift
+++ Pictory/Pictory/Controller/CreateViewController.swift
@@ -12,6 +12,17 @@ import JTAppleCalendar
 
 class CreateViewController: UIViewController {
     let dateFormatter = DateFormatter()
+    
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    @IBOutlet weak var month: UILabel!
+    
+    var firstDate: Date?
+    
+    let outsideMonthColor = UIColor.lightGray
+    let monthColor = UIColor.darkGray
+    let selectedMonthColor = UIColor.white
+    let currentDateSelectedViewColor = UIColor.white
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var timePicker: UIDatePicker!
     @IBOutlet weak var createBtn: UIButton!
@@ -19,8 +30,22 @@ class CreateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupCalendarView()
+    }
+    
+    func setupCalendarView() {
+        //Setup calendar spacing
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+        calendarView.scrollsToTop = false
+        
+        //Setup labels
+        calendarView.visibleDates {(visibleDates) in
+            self.setupViewsOfCalendar(from: visibleDates)
+        }
+        
+        calendarView.allowsMultipleSelection  = true
+        calendarView.isRangeSelectionUsed = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,17 +53,48 @@ class CreateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func handleCellSelected(view: JTAppleCell?, cellState: CellState) {
+        guard let validCell = view as? CustomCell else {return}
+        //selected one
+//        if cellState.isSelected {
+//            validCell.selectedView.isHidden = false
+//        } else {
+//            validCell.selectedView.isHidden = true
+//        }
+//        ramdom selected
+        switch cellState.selectedPosition() {
+        case .full, .left, .right:
+            validCell.selectedView.isHidden = false
+            validCell.selectedView.backgroundColor = UIColor.brown
+        case .middle:
+            validCell.selectedView.isHidden = false
+            validCell.selectedView.backgroundColor = UIColor.lightGray
+        default:
+            validCell.selectedView.isHidden = true
+            validCell.selectedView.backgroundColor = nil
+        }
     }
-    */
-
+    
+    func handleCelltextColor(view: JTAppleCell?, cellState: CellState) {
+        guard let validCell = view as? CustomCell else {return}
+        
+        if cellState.isSelected {
+            validCell.dateLabel.textColor = selectedMonthColor
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                validCell.dateLabel.textColor = monthColor
+            } else {
+                validCell.dateLabel.textColor = outsideMonthColor
+            }
+        }
+    }
+    
+    func setupViewsOfCalendar(from visibleDates: DateSegmentInfo) {
+        let date = visibleDates.monthDates.first!.date
+        
+        self.dateFormatter.dateFormat = "MMMM yyyy"
+        self.month.text = self.dateFormatter.string(from: date)
+    }
 }
 
 extension CreateViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
@@ -64,8 +120,17 @@ extension CreateViewController: JTAppleCalendarViewDelegate, JTAppleCalendarView
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
         sharedFunctionToConfigureCell(myCustomCell: cell, cellState: cellState, date: date)
-//        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
         cell.dateLabel.text = cellState.text
+        handleCellSelected(view: cell, cellState: cellState)
+//        calendar.selectDates(
+//            from: cellState.selectedPosition().left,
+//            to: cellState.selectedPosition().right,
+//            triggerSelectionDelegate: true,
+//            keepSelectionIfMultiSelectionAllowed: true)
+//        calendar.reloadData()
+        
+        
+        handleCelltextColor(view: cell, cellState: cellState)
         return cell
     }
     
@@ -73,5 +138,65 @@ extension CreateViewController: JTAppleCalendarViewDelegate, JTAppleCalendarView
         myCustomCell.dateLabel.text = cellState.text
     }
     
-
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        if firstDate != nil {
+            calendarView.selectDates(from: firstDate!, to: date,  triggerSelectionDelegate: false, keepSelectionIfMultiSelectionAllowed: true)
+        } else {
+            firstDate = date
+        }
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCelltextColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCelltextColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didScrollToDateSegmentWith visibleDates: DateSegmentInfo) {
+        self.setupViewsOfCalendar(from: visibleDates)
+    }
+    
+    // https://github.com/patchthecode/JTAppleCalendar/issues/244
+    func calendar(_ calendar: JTAppleCalendarView, shouldDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) -> Bool {
+        
+        let selectedDates = calendar.selectedDates
+        
+        if selectedDates.contains(date) {
+            // remove dates from the last selected.
+            if (selectedDates.count > 2 && selectedDates.first != date && selectedDates.last != date) {
+                let indexOfDate = selectedDates.index(of: date)
+                let dateBeforeDeselectedDate = selectedDates[indexOfDate!]
+                calendar.deselectAllDates()
+                calendar.selectDates(
+                    from: selectedDates.first!,
+                    to: dateBeforeDeselectedDate,
+                    triggerSelectionDelegate: true,
+                    keepSelectionIfMultiSelectionAllowed: true)
+                
+                calendar.reloadData()
+            }
+        }
+        return true
+    }
+//    func calendar(_ calendar: JTAppleCalendarView, shouldSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) -> Bool {
+//        let selectedDates = calendar.selectedDates
+//
+//        if selectedDates.contains(date) {
+//            if (selectedDates.count == 2 && selectedDates.first != date && selectedDates.last != date) {
+//
+//                let indexOfDate = selectedDates.index(of: date)
+//                let secondDate = selectedDates[indexOfDate!]
+//                calendar.deselectAllDates()
+//                calendar.selectDates(
+//                    from: selectedDates.first!,
+//                    to: secondDate,
+//                    triggerSelectionDelegate: true,
+//                    keepSelectionIfMultiSelectionAllowed: true)
+//
+//                calendar.reloadData()
+//            }
+//        }
+//        return true
+//    }
 }
