6f9b9ca0d1ae1764818fc70aaa950ba6e8835406
Jenna <jenna.lin@localz.co>
2018-06-20 16:19:56 +1000

    Add JTAppleCalendar to Create Journal page

--- Pictory/Pictory/Controller/CreateViewController.swift
+++ Pictory/Pictory/Controller/CreateViewController.swift
@@ -11,6 +11,7 @@ import Firebase
 import JTAppleCalendar
 
 class CreateViewController: UIViewController {
+    let dateFormatter = DateFormatter()
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var timePicker: UIDatePicker!
     @IBOutlet weak var createBtn: UIButton!
@@ -37,5 +38,40 @@ class CreateViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+extension CreateViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        dateFormatter.dateFormat = "YYYY-MM-dd HH:mm:ss Z"
+        dateFormatter.timeZone = Calendar.current.timeZone
+        dateFormatter.locale = Calendar.current.locale
+        
+        let startDate = dateFormatter.date(from: "2018-06-01 00:00:00 +0000")!
+        let endDate = dateFormatter.date(from: "2019-12-31 00:00:00 +0000")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = cell as! CustomCell
+        sharedFunctionToConfigureCell(myCustomCell: cell, cellState: cellState, date: date)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        sharedFunctionToConfigureCell(myCustomCell: cell, cellState: cellState, date: date)
+//        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+    
+    func sharedFunctionToConfigureCell(myCustomCell: CustomCell, cellState: CellState, date: Date) {
+        myCustomCell.dateLabel.text = cellState.text
+    }
+    
 
 }
---
+++ Pictory/Pictory/View/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  Pictory
+//
+//  Created by Jenna on 20/6/18.
+//  Copyright © 2018 Hsin-Ping Lin. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+}
